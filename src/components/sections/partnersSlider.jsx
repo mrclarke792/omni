792b4b41effa5619356a4e0fab18f1a6b3dfcfc8
mrclarke792 <mrclarke792@gmail.com>
2024-11-11 12:33:22 +0000

    Update project files

--- src/components/sections/partnersSlider.jsx
+++ src/components/sections/partnersSlider.jsx
@@ -1,38 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Autoplay } from "swiper/modules";
 
-
-const PartnersSlider = ({data}) => {
+const PartnersSlider = ({ data }) => {
     return (
         <div className="partner-slider-info" data-cue="slideInUp">
             <div className="partner-slider">
                 <Swiper
-                    spaceBetween={25}
-                    slidesPerView={5}
+                    spaceBetween={15}
+                    breakpoints={{
+                        320: { slidesPerView: 1 },   // Mobile portrait
+                        480: { slidesPerView: 2 },   // Mobile landscape
+                        768: { slidesPerView: 3 },   // Tablet
+                        1024: { slidesPerView: 4 },  // Small desktop
+                        1280: { slidesPerView: 5 },  // Larger screens
+                    }}
                     speed={1000}
                     autoplay={{
                         delay: 2000,
                     }}
                     loop={true}
-                    modules={[ Autoplay]}
+                    modules={[Autoplay]}
                 >
                     {
-                        data.map(({ id, link, src }) => {
-                            return (
-                                <SwiperSlide key={id}>
-                                    <div className="single-partner-logo">
-                                        <img src={src} alt={`partner-${id}`} />
-                                    </div>
-                                </SwiperSlide>
-                            )
-                        })
+                        data.map(({ id, link, src }) => (
+                            <SwiperSlide key={id}>
+                                <div className="single-partner-logo">
+                                    <img src={src} alt={`partner-${id}`} />
+                                </div>
+                            </SwiperSlide>
+                        ))
                     }
                 </Swiper>
             </div>
         </div>
-    )
+    );
 }
 
-export default PartnersSlider
+export default PartnersSlider;
